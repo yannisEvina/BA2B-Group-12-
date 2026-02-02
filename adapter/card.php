8679bc7c5b4b1f7c40a57726ed9cde3a06156099
alexioamana-afk <alexioamana@gmail.com>
2026-02-02 08:12:49 +0100

    Earvin added a code

---
+++ adapter/card.php
@@ -0,0 +1,36 @@
+<?php
+interface LocalATMCard {
+    public function insertToLocalATM(): string;
+}
+
+class InternationalCard {
+    public function insertToInternationalSlot(): string {
+        return "International card inserted using global slot.";
+    }
+}
+class CardAdapter implements LocalATMCard {
+
+    private InternationalCard $internationalCard;
+
+    public function __construct(InternationalCard $internationalCard) {
+        $this->internationalCard = $internationalCard;
+    }
+
+    public function insertToLocalATM(): string {
+        // Convert international action to local ATM compatibility
+        return $this->internationalCard->insertToInternationalSlot() . " Adapted for Local ATM!";
+    }
+}
+
+class LocalATM {
+    public function readCard(LocalATMCard $card): void {
+        echo $card->insertToLocalATM();
+    }
+}
+
+$atm = new LocalATM();
+$atm1= new LocalATM();
+$foreignCard = new InternationalCard();
+$adapter = new CardAdapter($foreignCard);
+
+$atm->readCard($adapter);
